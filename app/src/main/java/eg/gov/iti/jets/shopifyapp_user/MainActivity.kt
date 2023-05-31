2cd12affe063a865c3cdbeea7a9c8fd830fb1dd4
toqamohamed1999 <toqa111999mohamed@gmail.com>
2023-05-31 18:37:58 +0300

    Initial commit

---
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/MainActivity.kt
@@ -0,0 +1,11 @@
+package eg.gov.iti.jets.shopifyapp_user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
