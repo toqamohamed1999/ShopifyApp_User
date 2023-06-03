4e7a48d2981aaa8d5226855276d9f5f5903c5335
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-06-03 21:28:01 +0300

    Design Login UI add strings and drawables related to login design

---
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/auth/signUp/presentation/ui/SignUpFragment.kt
@@ -0,0 +1,21 @@
+package eg.gov.iti.jets.shopifyapp_user.auth.signUp.presentation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import eg.gov.iti.jets.shopifyapp_user.R
+
+class SignUpFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+}
