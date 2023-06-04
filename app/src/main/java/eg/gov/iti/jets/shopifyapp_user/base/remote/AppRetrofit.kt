6411a914a2c3ef06c4517520fff26afdd77b6b32
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-06-04 08:48:29 +0200

    retrofit base

--- app/src/main/java/eg/gov/iti/jets/shopifyapp_user/base/remote/AppRetrofit.kt
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/base/remote/AppRetrofit.kt
@@ -1,12 +1,12 @@
 package eg.gov.iti.jets.shopifyapp_user.base.remote
 
-import eg.gov.iti.jets.shopifyapp_user.util.Constants
+import eg.gov.iti.jets.shopifyapp_user.util.BASE_URL
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object AppRetrofit {
     val retrofit =  Retrofit.Builder()
-    .baseUrl(Constants.BASE_URL)
+    .baseUrl(BASE_URL)
     .addConverterFactory(GsonConverterFactory.create()).build()
 }
