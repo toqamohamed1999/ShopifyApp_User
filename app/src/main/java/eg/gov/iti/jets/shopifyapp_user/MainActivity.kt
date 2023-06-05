011bba1c6a34f888836da41208b2b0f3eace627d
Nada Ayman <nadaayman9997@gmail.com>
2023-06-05 11:44:27 +0300

    add bottom navigation and first design for home screen

--- app/src/main/java/eg/gov/iti/jets/shopifyapp_user/MainActivity.kt
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/MainActivity.kt
@@ -2,12 +2,46 @@ package eg.gov.iti.jets.shopifyapp_user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import eg.gov.iti.jets.shopifyapp_user.categories.presentation.CategoryFragment
+import eg.gov.iti.jets.shopifyapp_user.home.presentation.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottom_navigation) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.category -> {
+                    loadFragment(CategoryFragment())
+                    true
+                }
+                R.id.favorite -> {
+                    //loadFragment(FavoriteFragment())
+                    true
+                }
+                R.id.profile -> {
+                    //loadFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 
 }
---
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/home/presentation/HomeFragment.kt
@@ -0,0 +1,25 @@
+package eg.gov.iti.jets.shopifyapp_user.home.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import eg.gov.iti.jets.shopifyapp_user.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
