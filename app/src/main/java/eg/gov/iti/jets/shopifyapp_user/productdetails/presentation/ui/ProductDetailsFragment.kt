8a2bea7218c13007e1347f0f0ac885a31a17f065
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-06-12 15:20:19 +0300

    Design Product Details UI

---
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/productdetails/presentation/ui/ProductDetailsFragment.kt
@@ -0,0 +1,21 @@
+package eg.gov.iti.jets.shopifyapp_user.productdetails.presentation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import eg.gov.iti.jets.shopifyapp_user.R
+
+class ProductDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_product_details, container, false)
+    }
+
+
+}
