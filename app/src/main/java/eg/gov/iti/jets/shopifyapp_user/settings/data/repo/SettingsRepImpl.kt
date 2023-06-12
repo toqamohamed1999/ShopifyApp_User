28a4ded6390d5b6226bb4a6900f7486f968b5ff5
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-06-12 17:03:03 +0300

    setUp Sdk Paypal

--- app/src/main/java/eg/gov/iti/jets/shopifyapp_user/settings/data/repo/SettingsRepImpl.kt
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/settings/data/repo/SettingsRepImpl.kt
@@ -3,4 +3,19 @@ package eg.gov.iti.jets.shopifyapp_user.settings.data.repo
 import eg.gov.iti.jets.shopifyapp_user.settings.domain.repo.SettingRepo
 
 class SettingsRepImpl:SettingRepo {
+    override fun changeCurrency(currencyCode: String) {
+
+    }
+
+    override fun changePhoneNumber(newPhoneNumber: String) {
+
+    }
+
+    override fun getDefaultSettings(): Map<String, String> {
+        return mapOf()
+    }
+
+    override fun changeAddress(newShippingAddress: String) {
+
+    }
 }
