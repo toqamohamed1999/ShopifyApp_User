f4a89dc55aa23306fea85b2fece1b24329af5aa8
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-06-16 09:28:00 +0300

    change room with new pojo

--- app/src/main/java/eg/gov/iti/jets/shopifyapp_user/base/domain/repo/FavOpRepoInterface.kt
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/base/domain/repo/FavOpRepoInterface.kt
@@ -1,11 +1,12 @@
 package eg.gov.iti.jets.shopifyapp_user.base.domain.repo
 
+import eg.gov.iti.jets.shopifyapp_user.base.model.FavRoomPojo
 import eg.gov.iti.jets.shopifyapp_user.cart.domain.model.LineItem
 import kotlinx.coroutines.flow.Flow
 
 interface FavOpRepoInterface {
-    suspend fun insertFavProduct(lineItem: LineItem)
-    suspend fun deleteFavProductWithId(lineItemId: Long)
-    fun getAllFavProducts(): Flow<List<LineItem>>
-    fun getFavProductWithId(lineItemId: Long): Flow<LineItem>
+    suspend fun insertFavProduct(favRoomPojo: FavRoomPojo)
+    suspend fun deleteFavProductWithId(productId: Long)
+    fun getAllFavProducts(): Flow<List<FavRoomPojo>>
+    fun getFavProductWithId(productId: Long): Flow<FavRoomPojo >
 }
--- app/src/main/java/eg/gov/iti/jets/shopifyapp_user/base/repo/FavOpRepoImpl.kt
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/base/repo/FavOpRepoImpl.kt
@@ -3,24 +3,26 @@ package eg.gov.iti.jets.shopifyapp_user.base.repo
 import eg.gov.iti.jets.shopifyapp_user.base.domain.repo.FavOpRepoInterface
 import eg.gov.iti.jets.shopifyapp_user.base.local.database.favorite.FavLocalSourceImpl
 import eg.gov.iti.jets.shopifyapp_user.base.local.database.favorite.FavLocalSourceInterface
+import eg.gov.iti.jets.shopifyapp_user.base.model.FavRoomPojo
 import eg.gov.iti.jets.shopifyapp_user.cart.domain.model.LineItem
 import kotlinx.coroutines.flow.Flow
 
 class FavOpRepoImpl(private val favLocalSource: FavLocalSourceInterface = FavLocalSourceImpl()) :
     FavOpRepoInterface {
-    override suspend fun insertFavProduct(lineItem: LineItem) {
-        favLocalSource.insertFavProduct(lineItem)
+    override suspend fun insertFavProduct(favRoomPojo: FavRoomPojo) {
+        favLocalSource.insertFavProduct(favRoomPojo)
     }
 
-    override suspend fun deleteFavProductWithId(lineItemId: Long) {
-       favLocalSource.deleteFavProductWithId(lineItemId)
+    override suspend fun deleteFavProductWithId(productId: Long) {
+        favLocalSource.deleteFavProductWithId(productId)
     }
 
-    override fun getAllFavProducts(): Flow<List<LineItem>> {
+    override fun getAllFavProducts(): Flow<List<FavRoomPojo>> {
         return favLocalSource.getAllFavProducts()
     }
 
-    override fun getFavProductWithId(lineItemId: Long): Flow<LineItem> {
-        return favLocalSource.getFavProductWithId(lineItemId)
+    override fun getFavProductWithId(productId: Long): Flow<FavRoomPojo> {
+        return favLocalSource.getFavProductWithId(productId)
     }
+
 }
