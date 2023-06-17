815fc4fef8833f086762e09186d6798d08470b80
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-06-17 09:18:44 +0300

    handle viewModel and view for favorite room in Favorite fragment

--- app/src/main/java/eg/gov/iti/jets/shopifyapp_user/products/presentation/ui/OnClickProduct.kt
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/products/presentation/ui/OnClickProduct.kt
@@ -3,7 +3,7 @@ package eg.gov.iti.jets.shopifyapp_user.products.presentation.ui
 import eg.gov.iti.jets.shopifyapp_user.base.model.Product
 
 interface OnClickProduct{
-    fun onClickFavIcon(product : Product)
+    fun onClickFavIcon(product_Id : Long)
     fun onClickProductCard(product_Id : Long)
 
 }
