9dff95a6f3098aa841de2ab3306f90c3afb5b281
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-06-19 13:47:05 +0300

    google payment sandbox to paypal

---
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/payment/util/PaymentConstants.kt
@@ -0,0 +1,14 @@
+package eg.gov.iti.jets.shopifyapp_user.payment.util
+
+object PaymentConstants {
+   const val MerchantId ="jgzybzkpf9qfzfdk"
+   const val Public_key = "7hfgjpp9t9xqmrdk"
+   const val Private_key = "b0e02e882a5e5846a350684245c76857"
+
+   const val Tokenization_Key = "sandbox_gpwydry9_jgzybzkpf9qfzfdk"
+
+
+   const val CSEKey= "MIIBCgKCAQEAvo4t7u78xVp445xHkUjtJDibNLc09kwHTnZk8k7nfOpMEWn+IYP1i0fVG/L/WEEOC8GscfZWEJAaDFEM8lYNjkMss3CiJsYlG+AtQmUs6TProz+lDlS5sUB/I+dCGRGDKkHWsniIHKIFxlZ66wChkapT/CdXfKvtRbBUOMgibX4WwPv6/pMdvdvgOD5L8WlEX+gbYjxxwez4Nw3zXUGXV7cH45UUx1telCq2lZW3PXhHfDWPiB/PKNXSUwNSwYLFXEW2pnZyd4UU4tMbNO7QhRYsx1Rlo3nVB4a4ggjLkSxkPe1voJLDRr1rZolbCcJL2unFtvBZiQb45ZlogUPj/wIDAQAB"
+
+
+}
