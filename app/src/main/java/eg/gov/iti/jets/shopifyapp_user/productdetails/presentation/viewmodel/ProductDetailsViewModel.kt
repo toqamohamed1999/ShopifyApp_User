928370c109110b67fabe6c5f4ed0815d2bec749a
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-06-21 03:26:43 +0300

    SOLVE add to cart

--- app/src/main/java/eg/gov/iti/jets/shopifyapp_user/productdetails/presentation/viewmodel/ProductDetailsViewModel.kt
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/productdetails/presentation/viewmodel/ProductDetailsViewModel.kt
@@ -77,7 +77,7 @@ class ProductDetailsViewModel(
         var flag = false
         cartDraftOrder.draft_order?.line_items?.takeLast((cartDraftOrder.draft_order?.line_items?.size?:1)-1)?.forEach {
             if (product?.applied_discount?.description?.split(")")
-                    ?.get(0) == it.applied_discount.description?.split(")")?.get(0)
+                    ?.get(0) == (it?.applied_discount?.description?:"0.0").split(")")?.get(0)
             ) {
                 flag = true
                 if (it.quantity < quantity) {
