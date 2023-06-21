caaf8dfac36042b47dfd00bc9d46de74709186f1
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-06-21 19:03:56 +0300

    solve cart

--- app/src/main/java/eg/gov/iti/jets/shopifyapp_user/home/presentation/ui/CouponAdapter.kt
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/home/presentation/ui/CouponAdapter.kt
@@ -2,6 +2,7 @@ package eg.gov.iti.jets.shopifyapp_user.home.presentation.ui
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -10,6 +11,8 @@ import eg.gov.iti.jets.shopifyapp_user.R
 import eg.gov.iti.jets.shopifyapp_user.databinding.CouponImageBinding
 import eg.gov.iti.jets.shopifyapp_user.databinding.DicountItemBinding
 import eg.gov.iti.jets.shopifyapp_user.home.domain.model.addsmodels.DiscountCode
+import eg.gov.iti.jets.shopifyapp_user.settings.data.local.UserSettings
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
 
@@ -28,26 +31,65 @@ class CouponAdapter( var discounts: ArrayList<DiscountCode> , var myListener: Co
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val discount = discounts[position]
-        holder.binding.discountTitleTextView.text = discount.code
-        holder.binding.discountValueTextView.text = "${discount.created_at.replace("-","")} %"
-        val img = Random.nextInt(5)
-        when(img){
-            1->{
-                holder.binding.discountImageView.setImageDrawable(ResourcesCompat.getDrawable(holder.binding.root.resources,R.drawable.x1,null))
-            }
-            2->{
-                holder.binding.discountImageView.setImageDrawable(ResourcesCompat.getDrawable(holder.binding.root.resources,R.drawable.x2,null))
-            }
-            3->{
-                holder.binding.discountImageView.setImageDrawable(ResourcesCompat.getDrawable(holder.binding.root.resources,R.drawable.x3,null))
-            }
-            4->{
-                holder.binding.discountImageView.setImageDrawable(ResourcesCompat.getDrawable(holder.binding.root.resources,R.drawable.x4,null))
 
+        if(discount.updated_at=="percentage") {
+            holder.binding.imageViewdiscountImage.visibility=View.GONE
+            holder.binding.textViewvalueFixed.visibility=View.GONE
+            holder.binding.discountTitleTextView.text = discount.code
+            holder.binding.discountValueTextView.text = "${discount.created_at.replace("-", "")} %"
+            val img = Random.nextInt(5)
+            when (img) {
+                1 -> {
+                    holder.binding.discountImageView.setImageDrawable(
+                        ResourcesCompat.getDrawable(
+                            holder.binding.root.resources,
+                            R.drawable.x1,
+                            null
+                        )
+                    )
+                }
+                2 -> {
+                    holder.binding.discountImageView.setImageDrawable(
+                        ResourcesCompat.getDrawable(
+                            holder.binding.root.resources,
+                            R.drawable.x2,
+                            null
+                        )
+                    )
+                }
+                3 -> {
+                    holder.binding.discountImageView.setImageDrawable(
+                        ResourcesCompat.getDrawable(
+                            holder.binding.root.resources,
+                            R.drawable.x3,
+                            null
+                        )
+                    )
+                }
+                4 -> {
+                    holder.binding.discountImageView.setImageDrawable(
+                        ResourcesCompat.getDrawable(
+                            holder.binding.root.resources,
+                            R.drawable.x4,
+                            null
+                        )
+                    )
+
+                }
+                else -> {
+                    holder.binding.discountImageView.setImageDrawable(
+                        ResourcesCompat.getDrawable(
+                            holder.binding.root.resources,
+                            R.drawable.x5,
+                            null
+                        )
+                    )
+                }
             }
-            else->{
-                holder.binding.discountImageView.setImageDrawable(ResourcesCompat.getDrawable(holder.binding.root.resources,R.drawable.x5,null))
-            }
+        }else if(discount.updated_at=="fixed_amount")
+        {
+            holder.binding.discountTitleTextView.visibility=View.GONE
+            holder.binding.discountValueTextView.visibility=View.GONE
         }
         holder.binding.discountCardView.setOnClickListener {
             myListener.onImageClick(discount)
