b5512b474e8ca7a54ed77962c2315d71aa9748a3
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-06-21 09:58:54 +0300

    finish code coupon

--- app/src/main/java/eg/gov/iti/jets/shopifyapp_user/base/model/orders/CustomerOrder.kt
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_user/base/model/orders/CustomerOrder.kt
@@ -5,24 +5,5 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class CustomerOrder(
-    val accepts_marketing: Boolean = false,
-    val accepts_marketing_updated_at: String = "",
-    val admin_graphql_api_id: String = "",
-    val created_at: String = "",
-    val currency: String = "",
-    var default_address: ShippingAddress,
-    val email: String = "",
-    val first_name: String = "",
-    val id: Long = 0,
-    val last_name: String = "",
-    val marketing_opt_in_level: String = "",
-    val multipass_identifier: String = "",
-    val note: String = "",
-    val phone: String = "",
-    val sms_marketing_consent: String = "",
-    val state: String = "",
-    val tags: String = "",
-    val tax_exempt: Boolean = false,
-    val updated_at: String = "",
-    val verified_email: Boolean = false
+    val id: Long = 0
 ):Parcelable
